fc5df03a79123734fed8ca752da40d4024199bbf
alias-son <alias.mr.son@gmail.com>
2024-06-26 20:44:51 +0900

    Update App.svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,11 +1,21 @@
 <script>
-  let message;
-
-  fetch("http://127.0.0.1:8000/hello").then((response) => {
-    response.json().then((json) => {
-      message = json.message;
-    });
-  });
+  let name = 'world';
 </script>
 
-<h1>{message}</h1>
+<main>
+  <h1>Hello {name}!</h1>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+  }
+</style>
