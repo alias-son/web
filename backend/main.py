77ad5d98a225c20eb7cc26416a323dcc97e6f1e0
alias-son <alias.mr.son@gmail.com>
2024-06-26 19:11:36 +0900

    update

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI() 
+app = FastAPI()
 
-@app.get("/hello") 
-def hello_world(): 
-    return {"message": "Hello, world!"} 
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/hello")
+def hello():
+    return {"message": "안녕하세요 파이보"}
+
