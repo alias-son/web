4620f4b0cdae55d1d94f6d8227c6397bd472afb1
alias-son <alias.mr.son@gmail.com>
2024-06-27 15:30:06 +0900

    update

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,29 +1,11 @@
 <script>
-  let data = '';
+  let message;
 
-  const fetchData = async () => {
-    const response = await fetch('http://localhost:8000/api/data'); // FastAPI 서버 URL
-    const result = await response.json();
-    data = result.data;
-  }
-
-  fetchData();
+  fetch("http://127.0.0.1:8000/hello").then((response) => {
+    response.json().then((json) => {
+      message = json.message;
+    });
+  });
 </script>
 
-<main>
-  <h1>Data from FastAPI: {data}</h1>
-</main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-  }
-</style>
+<h1>{message}</h1>
